942b76bad7285dc144e2028acf6b8e628d1b0e08
Nathanael Wong <natgeo.wong@outlook.com>
2020-06-25 17:59:09 -0400

    Updating testresort functions

--- scripts/testresort.jl
+++ scripts/testresort.jl
@@ -7,3 +7,11 @@ tdir = "/n/holyscratch01/kuang_lab/nwong/sam_tmp"
 pdir = datadir()
 fnc  = "RCE_128x128x64_32"
 init,sroot = samstartup(tmppath=tdir,prjpath=pdir,config="RCETest",fname=fnc)
+# smod,spar,stime = saminitialize(init,modID="m2D",parID="prcp")
+
+samresort(init,sroot,modID="d2D",parID="t_sst")
+samresort(init,sroot,modID="m2D",parID="prcp")
+samresort(init,sroot,modID="m2D",parID="tcw")
+samresort(init,sroot,modID="m2D",parID="swp")
+samresort(init,sroot,modID="r2D",parID="sol_net_toa")
+samresort(init,sroot,modID="s3D",parID="t_abs")
