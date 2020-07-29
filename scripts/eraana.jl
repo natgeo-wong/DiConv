c12745cc28e6f2865bea54f88c37f98a399528b3
Nathanael Wong <natgeo.wong@outlook.com>
2020-07-29 02:25:45 -0400

    Update to include precipitation

--- scripts/eraana.jl
+++ scripts/eraana.jl
@@ -6,6 +6,8 @@ using ClimateERA
 init,eroot = erastartup(aID=1,dID=1,path="/n/kuangdss01/lab/");
 eraanalysis(init,eroot,modID="msfc",parID="t_sst",regID="TRP");
 # eraanalysis(init,eroot,modID="msfc",parID="t_sfc",regID="TRP");
+# eraanalysis(init,eroot,modID="msfc",parID="prcp_tot",regID="TRP",timeID=[1980,2019]);
 
 eracompile(init,eroot,modID="msfc",parID="t_sst",regID="TRP");
 eracompile(init,eroot,modID="msfc",parID="t_sfc",regID="TRP");
+eracompile(init,eroot,modID="msfc",parID="prcp_tot",regID="TRP",timeID=[1980,2019]);
