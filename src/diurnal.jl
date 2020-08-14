dd987d35892dcc7ba0fed87ec18023c489808294
Nathanael Wong <natgeo.wong@outlook.com>
2020-08-14 16:53:22 -0400

    Added insolation plot

--- src/diurnal.jl
+++ src/diurnal.jl
@@ -50,6 +50,22 @@ function gettcw(;
 
 end
 
+function getsol(;
+    experiment::AbstractString, config::AbstractString
+)
+
+    init,sroot = samstartup(
+        tmppath="",prjpath=datadir(),fname="RCE_*",
+        experiment=experiment,config=config,welcome=false
+    )
+
+    tcw,t2D = domainmean_timeseries(init,sroot,modID="r2D",parID="sol_net_toa");
+    tstep = convert(Integer,round(1/(t2D[2]-t2D[1])))
+
+    return diurnal(tcw,tstep),tstep
+
+end
+
 function diurnal(var::Vector{<:Real},tstep::Integer)
 
     lt = length(var); nday = floor(Integer,lt/tstep)
